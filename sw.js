b8626a2834ccfcd777bb305e19c2432e118fbf49
Voks <vishnuba21112@bmscl.ac.in>
2026-09-15 15:59:38 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+const CACHE="case-ledger-v1";self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","./index.html","./manifest.json"]))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(x=>{let c=x.clone();caches.open(CACHE).then(k=>k.put(e.request,c));return x}).catch(()=>caches.match("./index.html")))));
